7df967dd0af755e234785b0b95f753c64c66fbba
Matt Nyaseda <mattkeneth@gmail.com>
2023-11-14 13:52:05 -0800

    errors

--- 04-_our_printf.c
+++ 04-_our_printf.c
@@ -14,11 +14,11 @@ int _printf(const char *format, ...)
 	int argc = 0;
 	va_list c_n;
 
-	va_start(c_n, *format);
+	va_start(c_n, format);
 
 	while (*format != '\0')
 	{
-		if (*format != '%')
+		if (*format == '%')
 		{
 			format++;
 			switch (*format)
@@ -26,9 +26,7 @@ int _printf(const char *format, ...)
 			case 'c':
 			{
 				char ract = va_arg(c_n, int);
-
 				format++;
-
 				_putchar(ract);
 				argc++;
 			}
@@ -37,7 +35,6 @@ int _printf(const char *format, ...)
 			case 's':
 			{
 				char *sent = va_arg(c_n, char *);
-
 				format++;
 				_printsString(sent);
 				argc += _strlen(sent);
@@ -55,9 +52,7 @@ int _printf(const char *format, ...)
 			case 'd':
 			{
 				int new = va_arg(c_n, int);
-
 				format++;
-
 				_printsNumbers(new);
 				argc++;
 			}
@@ -65,9 +60,7 @@ int _printf(const char *format, ...)
 
 			case 'i':
 			{
-
 				int new = va_arg(c_n, int);
-
 				format++;
 				_printsNumbers(new);
 				argc++;
@@ -75,8 +68,7 @@ int _printf(const char *format, ...)
 			break;
 
 			default:
-				perror("_putchar");
-				_putchar('\n');
+				fprintf(stderr, "Error: Unknown format\n");
 				return (-1);
 				break;
 			}
